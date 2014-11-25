ca01c055e3a1ef55d75efdaa2cb10d24739ddd27
Ben Tranter <bwtrante@lakeheadu.ca>
2014-11-24 21:25:37 -0500

    Add project
    Lol Meteor

---
+++ residence-manager/client/templates/posts/post_item.js
@@ -0,0 +1,51 @@
+var POST_HEIGHT = 80;
+var Positions = new Meteor.Collection(null);
+
+Template.postItem.helpers({
+  ownPost: function() {
+    return this.userId == Meteor.userId();
+  },
+  isAdmin: function() {
+    if (Meteor.userId())
+      return Meteor.user().username == 'admin'; // If your username is admin, you have full priveleges. Not very secure
+  },
+  upvotedClass: function() {
+    var userId = Meteor.userId();
+    if (userId && !_.include(this.upvoters, userId)) {
+      return 'upvotable';
+    } else {
+      return 'voting-disabled';
+    }
+  },
+  attributes: function() {
+    var post = _.extend({}, Positions.findOne({postId: this._id}), this);
+    var newPosition = post._rank * POST_HEIGHT;
+    var attributes = {};
+    
+    if (_.isUndefined(post.position)) {
+      attributes.class = 'post invisible';
+    } else {
+      var delta = post.position - newPosition;      
+      attributes.style = "top: " + delta + "px";
+      if (delta === 0)
+        attributes.class = "post animate"
+    }
+    
+    Meteor.setTimeout(function() {
+      Positions.upsert({postId: post._id}, {$set: {position: newPosition}})
+    });
+    
+    return attributes;
+  }
+});
+
+Template.postItem.events({
+  'click .upvotable': function(e) {
+    e.preventDefault();
+    Meteor.call('upvote', this._id);
+  },
+  'click .delete-post': function(e) {
+    e.preventDefault();
+    Meteor.call('delete', this._id);
+  }
+});
